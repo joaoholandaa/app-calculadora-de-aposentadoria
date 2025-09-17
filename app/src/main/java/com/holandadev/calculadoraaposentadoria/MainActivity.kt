bb4e4c5ff5704a6512b373d369d521ae2502e5a6
joaoholandaa <joaocmdt505@gmail.com>
2025-09-16 21:57:30 -0300

    feat(main): criar MainActivity

---
+++ app/src/main/java/com/holandadev/calculadoraaposentadoria/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.holandadev.calculadoraaposentadoria
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
