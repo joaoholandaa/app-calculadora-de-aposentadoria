0a93caf7500a3b809e80f66a2849d2ebfba5ae9d
joaoholandaa <joaocmdt505@gmail.com>
2025-09-18 17:44:09 -0300

    Rfix: corrigir action da MainActivity no manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.holandadev.calculadoraaposentadoria"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.holandadev.calculadoraaposentadoria"
